f17f0abf0b698db47a649270c3d9710991bba59d
Nguyễn Bá Thái <thai.nguyen@aegona.com>
2022-11-30 19:21:10 +0700

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+const prerender = require("prerender");
+var server = prerender({
+    chromeLocation: "C:/Program Files/Google/Chrome/Application/chrome.exe",
+    pageLoadTimeout: 100 * 1000,
+});
+
+server.use(require("prerender-memory-cache"));
+// server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.browserForceRestart());
+// server.use(prerender.blockResources());
+// // server.use(prerender.removeScriptTags());
+// server.use(prerender.httpHeaders());
+
+server.start();
